c519f5b21cfdcbe28c50468e8824be13d4fa654e
Peng <wpcfan@gmail.com>
2023-12-29 16:08:46 +0800

    add group to list

--- lib/todos/components/todo_list_widget.dart
+++ lib/todos/components/todo_list_widget.dart
@@ -19,19 +19,76 @@ class TodoListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Map<String, List<Todo>> groupedTodos = {};
+
+    // Group todos by weekday
+    for (var todo in todos) {
+      String weekday = todo.dueDate!.weekday.toString();
+      if (!groupedTodos.containsKey(weekday)) {
+        groupedTodos[weekday] = [];
+      }
+      groupedTodos[weekday]!.add(todo);
+    }
+
     return SliverList.builder(
       key: key,
-      itemCount: todos.length,
+      itemCount: groupedTodos.length,
       itemBuilder: (context, index) {
-        final todo = todos[index];
-        return TodoItemWidget(
-          key: Key('__todo_item_${todo.id}__'),
-          todo: todo,
-          onToggle: onToggle,
-          onEdit: onEdit,
-          onDelete: onDelete,
+        final weekday = groupedTodos.keys.toList()[index];
+        final todosForWeekday = groupedTodos[weekday]!;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                _getWeekdayName(int.parse(weekday)),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: todosForWeekday.length,
+              itemBuilder: (context, index) {
+                final todo = todosForWeekday[index];
+                return TodoItemWidget(
+                  key: Key('__todo_item_${todo.id}__'),
+                  todo: todo,
+                  onToggle: onToggle,
+                  onEdit: onEdit,
+                  onDelete: onDelete,
+                );
+              },
+            ),
+          ],
         );
       },
     );
+  }
+
+  String _getWeekdayName(int weekday) {
+    switch (weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return '';
+    }
   }
 }
